1d92c94116d0662431c262e20b01e1b1668c62c2
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-10-16 05:16:17 +0100

    Update to-do.js
    not yet giving

--- To-do list/to-do.js
+++ To-do list/to-do.js
@@ -1,5 +1,45 @@
 const todoItems = document.getElementsByClassName("ToDoItems")[0];
 const input = document.getElementById('userInput');
-const trahIcon = document.getElementById("trash");
+const trashIcon = document.getElementById("trash");
+
+input.addEventListener("keydown", function(event){
+    if( event.key === "Enter" ){
+        additems();
+    }
+});
+
+function additems(){
+    var divParent = document.createElement("div");
+    var divChild = document.createElement("div");
+    var checkIcon = documemt.createElement("i");
+    var trashIcon = documemt.createElement("i");
+
+    divParent.className = "item";
+    divParent.innerHTML = '<div>'+input.value+'</div>';
+
+    checkIcon.className = "fa-regular fa-square-check";
+    checkIcon.style.color = "lightgray";
+    checkIcon.addEventListener("click",function(){
+        checkIcon.style.color = 'limegreen'
+    });
+
+    divChild.appendChild(checkIcon);
+
+    trashIcon.classname = "fa-solid fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click",function(){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
+
+    divParent.appendChild(divChild);
+
+    todoItems.appendChild(divParent);
+
+    input.value = '';
+
+}
+
 
 
